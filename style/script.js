0c89f9ade9238a510b6d39c9c7d670f945fe1fb0
millahuyskens <milla.huyskens@student.pxl.be>
2024-06-06 17:01:50 +0200

    finale

--- style/script.js
+++ style/script.js
@@ -2,9 +2,25 @@ alert("I hope today is better than yesterday");
 
 let currentSlide = 0;
 
-function moveSlide(step) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-    currentSlide = (currentSlide + step + totalSlides) % totalSlides;
-    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
 }
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 2500);
